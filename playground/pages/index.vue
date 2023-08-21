e2248342fb913d47539b06d6f3bbaebf300f5730
wiidede <wiixdede@gmail.com>
2023-08-21 09:38:29 +0800

    feat: demo add notification and message

--- playground/pages/index.vue
+++ playground/pages/index.vue
@@ -3,23 +3,51 @@ const disabled = ref(false)
 const form = reactive({
   name: '',
 })
+
+function handleMessage() {
+  Message.info('This is an info message')
+}
+function handleNotification() {
+  Notification.success('This is a success message!')
+}
 </script>
 
 <template>
-  <icon-thumb-up-fill />
-  <icon-heart-fill />
-  <icon-star-fill />
-  <a-space style="margin-bottom: 20px;">
-    <a-switch v-model="disabled" />
-    Disabled: {{ disabled }}
-  </a-space>
-  <AForm :model="form" :disabled="disabled" :style="{ width: '600px' }">
-    <AFormItem
-      field="name" label="Username"
-      :rules="[{ required: true, message: 'name is required' }, { minLength: 5, message: 'must be greater than 5 characters' }]"
-    >
-      <MyInput v-model="form.name" placeholder="please enter your username..." />
-      (useFormItem)
-    </AFormItem>
-  </AForm>
+  <div :style="{ display: 'flex', justifyContent: 'space-around' }">
+    <a-button @click="handleMessage">
+      Info Message
+    </a-button>
+    <a-button status="success" @click="handleNotification">
+      Success Notification
+    </a-button>
+  </div>
+  <a-divider />
+  <a-row :gutter="24">
+    <a-col :span="12">
+      <a-card :style="{ width: '360px', margin: 'auto' }" title="Arco Card">
+        <template #extra>
+          <a-link>More</a-link>
+        </template>
+        <icon-thumb-up-fill />
+        <icon-heart-fill />
+        <icon-star-fill />
+      </a-card>
+    </a-col>
+    <a-col :span="12">
+      <a-card :style="{ width: '360px', margin: 'auto' }" title="useFormItem">
+        <a-space style="margin-bottom: 20px;">
+          <a-switch v-model="disabled" />
+          Disabled: {{ disabled }}
+        </a-space>
+        <AForm :model="form" :disabled="disabled" :style="{ width: '600px' }">
+          <AFormItem
+            field="name" label="Username"
+            :rules="[{ required: true, message: 'name is required' }, { minLength: 5, message: 'must be greater than 5 characters' }]"
+          >
+            <MyInput v-model="form.name" placeholder="please enter your username..." />
+          </AFormItem>
+        </AForm>
+      </a-card>
+    </a-col>
+  </a-row>
 </template>
